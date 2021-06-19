96d81ed5bab363d3bff12a70c28196b4e74e559c
Just van den Broecke <just@justobjects.nl>
2021-06-19 14:06:42 +0200

    fix hostname changes and #29 ignore GPKG -shm and -wal files for Git

--- services/env.sh
+++ services/env.sh
@@ -10,21 +10,31 @@ export TRAEFIK_SSL_ENDPOINT=
 export TRAEFIK_SSL_DOMAIN="apitestbed.geonovum.nl"
 export TRAEFIK_SSL_CERT_RESOLVER=
 export TRAEFIK_USE_TLS="false"
+export HOST_UID=$(id -u)
+export HOST_GID=$(id -g)
+export HOST_UID_GID="${HOST_UID}:${HOST_GID}"
 
 # Set host-dependent vars
 case "${HOSTNAME}" in
     "OGCAPIP")
         DEPLOY_ENV="prod"
-        TRAEFIK_SSL_ENDPOINT="https"
-        TRAEFIK_SSL_CERT_RESOLVER="le"
-        TRAEFIK_USE_TLS="true"
+        ;;
+    "apitestbed.geonovum.nl")
+        DEPLOY_ENV="prod"
+        ;;
+    "apitestbed")
+        DEPLOY_ENV="prod"
         ;;
     "vps11118.alm01.cloud")
         DEPLOY_ENV="prod"
-        TRAEFIK_SSL_ENDPOINT="https"
-        TRAEFIK_SSL_CERT_RESOLVER="le"
-        TRAEFIK_USE_TLS="true"
         ;;
     *)
         echo "Default Local Host ${HOSTNAME}"
 esac
+
+if [[ ${DEPLOY_ENV} = "prod" ]]
+then
+    TRAEFIK_SSL_ENDPOINT="https"
+    TRAEFIK_SSL_CERT_RESOLVER="le"
+    TRAEFIK_USE_TLS="true"
+fi
