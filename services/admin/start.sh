9538ff90a2cd5df7ef324dd9c15a2bf0149740cf
Just van den Broecke <just@justobjects.nl>
2021-06-22 13:58:33 +0200

    #30 env var propagation - fix var check admin

--- services/admin/start.sh
+++ services/admin/start.sh
@@ -2,8 +2,8 @@
 
 source ../env.sh
 
-[[ ${DEPLOY_ENV} == "prod" ]] && [[ -z ${GHC_PG_USER} ]] && echo "EXIT: Admin creds must be set for prod server!" && exit -1
-echo "Looks good! POSTGRES user is: ${GHC_PG_USER}"
+[[ ${DEPLOY_ENV} == "prod" ]] && [[ -z ${GHC_ADMIN_USER_NAME} ]] && echo "EXIT: Admin creds must be set for prod server!" && exit -1
+echo "Looks good! GHC_ADMIN_USER_NAME user is: ${GHC_ADMIN_USER_NAME}"
 
 ./stop.sh
 docker-compose up -d
