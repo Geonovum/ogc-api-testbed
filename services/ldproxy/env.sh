4bfc3e12dafdb549dd9e153c8082ec066213f29f
Just van den Broecke <just@justobjects.nl>
2021-06-28 12:41:16 +0200

    #32 env for all services - ldproxy fix space

--- services/ldproxy/env.sh
+++ services/ldproxy/env.sh
@@ -10,6 +10,6 @@ export SERVICE_PATH="/${SERVICE_NAME}"
 export SERVICE_PATH_MANAGER="/manager_ldproxy"
 
 # Can be usually left as isunless you want to run specific build and/or version.
-export DOCKER_IMAGE_NAME=" iide/ldproxy"
+export DOCKER_IMAGE_NAME="iide/ldproxy"
 export DOCKER_IMAGE_VERSION="master"
 export DOCKER_IMAGE="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
