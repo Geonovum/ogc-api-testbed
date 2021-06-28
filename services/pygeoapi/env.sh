1db21fb0232c30852169d0c77feefb1459d323a5
Just van den Broecke <just@justobjects.nl>
2021-06-28 13:09:19 +0200

    #32 env for all services - fix pygeoapi

--- services/pygeoapi/env.sh
+++ services/pygeoapi/env.sh
@@ -5,8 +5,8 @@ source ../env.sh
 # Change for new service and subpath
 export SERVICE_NAME="pygeoapi"
 
-# Is subpath (Python web apps only).
-export SCRIPT_NAME="/${SERVICE_NAME}"
+# Is subpath (becomes SCRIPT_NAME for Python web apps only).
+export SERVICE_PATH="/${SERVICE_NAME}"
 
 # Can be usually left as isunless you want to run specific build and/or version.
 export DOCKER_IMAGE_NAME="geopython/pygeoapi"
