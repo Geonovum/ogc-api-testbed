301b796ff5e32c19d23d1900fb913577b4483c6e
Just van den Broecke <just@justobjects.nl>
2021-06-30 15:32:15 +0200

    #36 ignore error exit codes within clean.sh

--- services/clean.sh
+++ services/clean.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+#
+# Cleanup dangling Docker stuff, ignore any errors.
+#
+# Just van den Broecke - 2021
+#
 
 # Remove all exited containers
 for c in $(docker ps -a -f status=exited -q)
 do
-	docker rm ${c}
+	docker rm ${c} 2>/dev/null | true
 done
 
 # And dangling images
 for i in $(docker images -f dangling=true -q)
 do
-	docker rmi ${i}
+	docker rmi ${i} 2>/dev/null | true
 done
