04e491801e44b000733f9eda99f07eae807aafd4
Just van den Broecke <just@justobjects.nl>
2023-03-14 19:46:23 +0100

    #51 crs-features-ogc branch from pygeoapi repo as Docker dev image

--- services/pygeoapi/env.sh
+++ services/pygeoapi/env.sh
@@ -10,10 +10,10 @@ export SERVICE_PATH="/${SERVICE_NAME}"
 
 # Can be usually left as is, unless you want to run specific build and/or version.
 export DOCKER_IMAGE_NAME="geopython/pygeoapi"
-export DOCKER_IMAGE_VERSION="latest"
-# export DOCKER_IMAGE_VERSION="dev"
+# export DOCKER_IMAGE_VERSION="latest"
+export DOCKER_IMAGE_VERSION="dev"
 export DOCKER_IMAGE="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
 
-export PYGEOAPI_REPO="https://github.com/justb4/pygeoapi"
-export PYGEOAPI_BRANCH="1128-oapif-ogc-crs"
+export PYGEOAPI_REPO="https://github.com/geopython/pygeoapi"
+export PYGEOAPI_BRANCH="crs-features-ogc"
 export PYGEOAPI_REPO_ZIP="${PYGEOAPI_REPO}/archive/refs/heads/${PYGEOAPI_BRANCH}.zip"
