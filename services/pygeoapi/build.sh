9ec9d82f53d65edd02db690d96b71f594536c087
Just van den Broecke <just@justobjects.nl>
2023-03-14 15:12:45 +0100

    #51 add build for pygeoapi branch

--- services/pygeoapi/build.sh
+++ services/pygeoapi/build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+#
+# Build script for branch version of pygeoapi Docker Image
 
 source ./env.sh
 
-./stop.sh && docker pull ${DOCKER_IMAGE} && ./start.sh
+[[ -z ${PYGEOAPI_BRANCH} ]] && echo "PYGEOAPI_BRANCH var not set - exit" && exit 1
+
+/bin/rm ${PYGEOAPI_BRANCH}.zip > /dev/null 2>&1
+/bin/rm -rf ${PYGEOAPI_BRANCH} > /dev/null 2>&1
+
+wget ${PYGEOAPI_REPO_ZIP}
+unzip -d ${PYGEOAPI_BRANCH} ${PYGEOAPI_BRANCH}.zip
+cd ${PYGEOAPI_BRANCH} || exit
+
+export DOCKER_IMAGE_VERSION="dev"
+
+docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
--- services/pygeoapi/env.sh
+++ services/pygeoapi/env.sh
@@ -8,7 +8,11 @@ export SERVICE_NAME="pygeoapi"
 # Is subpath (becomes SCRIPT_NAME for Python web apps only).
 export SERVICE_PATH="/${SERVICE_NAME}"
 
-# Can be usually left as isunless you want to run specific build and/or version.
+# Can be usually left as is, unless you want to run specific build and/or version.
 export DOCKER_IMAGE_NAME="geopython/pygeoapi"
 export DOCKER_IMAGE_VERSION="latest"
 export DOCKER_IMAGE="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
+
+export PYGEOAPI_REPO="https://github.com/justb4/pygeoapi"
+export PYGEOAPI_BRANCH="1128-oapif-ogc-crs"
+export PYGEOAPI_REPO_ZIP="${PYGEOAPI_REPO}/archive/refs/heads/${PYGEOAPI_BRANCH}.zip"
